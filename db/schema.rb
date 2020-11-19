28b2cb89372a8f9d75718d3f2dbe72ad75c1e237
Nicolas jose Dos santos <nicolas.dossants@laposte.net>
2020-11-19 10:16:02 +0100

    schema movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_141049) do
+ActiveRecord::Schema.define(version: 2020_11_13_153121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
