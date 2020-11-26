7ce313f605b492e2807c915245d20a38727cc2f5
gamegine <gamegine@protonmail.com>
2020-11-26 22:37:25 +0100

    test movies controler results

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -15,8 +15,13 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get movies_url
+    EXPECTED = Movie.all.map { |e| movie_to_json e }
+
+    get movies_url('json')
     assert_response :success
+    # json_response = JSON.parse(response.body)
+    json_response = ActiveSupport::JSON.decode @response.body
+    assert_equal json_response, EXPECTED
   end
 
   test "should get new" do
@@ -25,16 +30,22 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create movie" do
-    assert_difference('Movie.count') do
-      post movies_url, params: { movie: { title: @movie.title } }
+    assert_difference('Movie.count', 1) do
+      post movies_url, params: { movie: { title: "@movie.title" } }
     end
 
     assert_redirected_to movie_url(Movie.last)
+    assert_equal Movie.last.title, "@movie.title"
   end
 
   test "should show movie" do
-    get movie_url(@movie)
+    EXPECTED = movie_to_json @movie
+
+    get movie_url({:id=>@movie.id, format: :json})
     assert_response :success
+    # json_response = JSON.parse(response.body)
+    json_response = ActiveSupport::JSON.decode @response.body
+    assert_equal json_response, EXPECTED
   end
 
   test "should get edit" do
@@ -43,15 +54,39 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update movie" do
-    patch movie_url(@movie), params: { movie: { title: @movie.title } }
-    assert_redirected_to movie_url(@movie)
+    movie = Movie.create({
+        title: "Movie Title",
+        user: users(:one)
+    })
+
+    assert_changes "Movie.last.title", from: "Movie Title", to: "test" do
+      patch movie_url(movie), params: { movie: { title: "test" } }
+    end
+
+    assert_redirected_to movie_url(movie)
   end
 
   test "should destroy movie" do
+    movie = Movie.create({
+        title: "Movie Title",
+        user: users(:one)
+    })
     assert_difference('Movie.count', -1) do
-      delete movie_url(@movie)
+      delete movie_url(movie)
     end
 
     assert_redirected_to movies_url
   end
+
+  private
+
+    # Movie.all.as_json(except: [:user_id])
+    #       .each { |e| e['url'] = movie_url({:id=>e['id'], format: :json}) }
+    #       .each { |e| e['url'] = "http://www.example.com/movies/#{e['id']}.json" }
+
+    def movie_to_json (model)
+      res = model.as_json(except: [:user_id])
+      res['url'] = movie_url({:id=>res['id'], format: :json})
+      return res
+    end
 end
