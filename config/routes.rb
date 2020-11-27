3987f798cba64c8d56127478f559859f7d1d5652
Nicolas jose Dos santos <nicolas.dossants@laposte.net>
2020-11-27 16:32:36 +0100

    search controller api (filter)

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'movies#index'
   resources :movies
-  get    '/title'      => 'movies#searchTitle'
-  get    '/date'        => 'movies#searchDate'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :movies
+      get    'search/title'      => 'searches#searchTitle'
+      get    'search/date'        => 'searches#searchDate'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
