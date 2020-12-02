793ce1bb4f97946cd002169ae052a66a2b92946d
gamegine <gamegine@protonmail.com>
2020-12-02 23:05:49 +0100

    token workflow

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
       get    'search/date'        => 'searches#searchDate'
     end
   end
+  get    'token'      => 'token#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
